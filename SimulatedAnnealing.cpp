fc5fa8e6c7ad0f51e9dca04a703bcc1d89686e88
Florian Peeters <jeanflo125@gmail.com>
2019-10-21 19:19:28 +0200

    Start of transition to ProblemGraph

--- SimulatedAnnealing.cpp
+++ SimulatedAnnealing.cpp
@@ -5,47 +5,157 @@
 
 typedef std::mt19937_64 Rng;
 
-void initRandomColoring(Graph &graph, Rng &rng) {
-    std::uniform_int_distribution<unsigned int> distribution(1, graph.getNbColors());
+ProblemGraph::ProblemGraph(Graph &graph) : graph(graph), nonFixedNodes(), removedGroups(), labels(nullptr) {
+    labels = (Label*) malloc(sizeof(Label) * graph.getNbNodes());
+    memset(labels, NONFIXED, sizeof(Label) * graph.getNbNodes());
+
     for (unsigned int node = 0; node < graph.getNbNodes(); ++node) {
-        if (!graph.isPreColored(node))
-            graph.color(node, distribution(rng));
+        if (labels[node] != NONFIXED)
+            continue; // Already done
+
+        if (graph.isPreColored(node)) {
+            labels[node] = PRECOLORED;
+            continue;
+        }
+        if (graph.getEdges(node).size() == 1) {
+            RemovedGroup removedGroup(node);
+            labels[node] = REMOVED;
+
+            unsigned int previous = node;
+            unsigned int next = graph.getEdges(node)[0];
+            while (next != -1) {
+                const std::vector<unsigned int> &vector = graph.getEdges(next);
+                if (!graph.isPreColored(next) && vector.size() == 2) {
+                    removedGroup.nodes.push_back(next);
+                    labels[next] = REMOVED;
+
+                    if (vector[0] == previous) {
+                        previous = next;
+                        next = vector[1];
+                    } else {
+                        previous = next;
+                        next = vector[0];
+                    }
+                } else {
+                    removedGroup.connection = next;
+                    break;
+                }
+            }
+            removedGroups.push_back(removedGroup);
+            continue;
+        }
+        if (graph.getEdges(node).size() == 2) {
+            if (graph.isPreColored(graph.getEdges(node)[0]) && graph.isPreColored(graph.getEdges(node)[1])) {
+                enclosedNodes.push_back(node);
+                labels[node] = ENCLOSED;
+            }
+            continue;
+        }
+    }
+
+    for (unsigned int node = 0; node < graph.getNbNodes(); ++node) {
+        if (labels[node] == NONFIXED)
+            nonFixedNodes.push_back(node);
     }
 }
 
-std::vector<Group> generateGroups(const Graph &graph) {
-    std::vector<Group> groups;
-    int group[graph.getNbNodes()];
-    memset(&group, 0, sizeof(int) * graph.getNbNodes());
-    int groupCounter = 0;
+unsigned int ProblemGraph::getHappyVertices() const {
+    unsigned int nbHappy = 0;
+
     for (unsigned int node = 0; node < graph.getNbNodes(); ++node) {
-        if (graph.isPreColored(node)) {
-            group[node] = -1;
-            continue;
+        bool happy;
+        switch (labels[node]) {
+            case PRECOLORED:
+            case NONFIXED:
+                happy = true;
+                for (unsigned int adj : graph.getEdges(node)) {
+                    switch (labels[adj]) {
+                        case PRECOLORED:
+                        case NONFIXED:
+                            if (graph.getColor(adj) != graph.getColor(node))
+                                happy = false;
+                            break;
+                        case REMOVED:
+                            break;
+                        case ENCLOSED:
+                            happy = false; //TODO: improve enclosed counting
+                            break;
+                    }
+                    if (!happy)
+                        break;
+                }
+                if (happy)
+                    ++nbHappy;
+                break;
+            case REMOVED:
+                ++nbHappy;
+                break;
+            case ENCLOSED:
+                break;
         }
-        if (group[node] != 0)
+    }
+
+    return nbHappy;
+}
+
+unsigned int ProblemGraph::colorRemoved() {
+    for (const RemovedGroup &removedGroup: removedGroups) {
+        for (unsigned int node: removedGroup.nodes) {
+            graph.color(node, graph.getColor(removedGroup.connection));
+        }
+    }
+
+    for (unsigned int enclosedNode: enclosedNodes) {
+        // TODO
+    }
+
+    return graph.getHappyVertices();
+}
+
+void initRandomColoring(ProblemGraph &pGraph, Rng &rng) {
+    std::uniform_int_distribution<unsigned int> distribution(1, pGraph.graph.getNbColors());
+    for (unsigned int node: pGraph.nonFixedNodes) {
+        if (!pGraph.graph.isPreColored(node))
+            pGraph.graph.color(node, distribution(rng));
+    }
+}
+
+std::vector<Group> generateGroups(const ProblemGraph &pGraph) {
+    std::vector<Group> groups;
+
+    int group[pGraph.nonFixedNodes.size()];
+    memset(&group, 0, sizeof(int) * pGraph.nonFixedNodes.size());
+
+    int groupCounter = 0;
+
+    for (unsigned int nodeIdx = 0; nodeIdx < (unsigned int)pGraph.nonFixedNodes.size(); ++nodeIdx) {
+        if (group[nodeIdx] != 0)
             continue;
 
-        Group newGroup(graph.getColor(node), node);
-        group[node] = ++groupCounter;
+        unsigned int node = pGraph.nonFixedNodes[nodeIdx];
+
+        Group newGroup(pGraph.graph.getColor(node), node);
+        group[nodeIdx] = ++groupCounter;
         std::queue<unsigned int> toCheck;
         toCheck.push(node);
 
         while (!toCheck.empty()) {
             const unsigned int check = toCheck.front();
             toCheck.pop();
-            for (unsigned int adj: graph.getEdges(check)) {
-                if (graph.getColor(adj) != newGroup.color)
-                    newGroup.adjColors.insert(graph.getColor(adj));
-
-                if (graph.getColor(adj) != newGroup.color || group[adj] != 0)
-                    continue;
-                if (graph.isPreColored(adj)) {
-                    group[adj] = -1;
+            for (unsigned int adj: pGraph.graph.getEdges(check)) {
+                if (pGraph.graph.getColor(adj) != newGroup.color) {
+                    newGroup.adjColors.insert(pGraph.graph.getColor(adj));
                     continue;
                 }
 
-                group[adj] = groupCounter;
+                if (pGraph.graph.isPreColored(adj))
+                    continue;
+
+                if (pGraph.labels[adj] == NONFIXED) {
+                    auto adjIdx = *std::find(pGraph.nonFixedNodes.begin(), pGraph.nonFixedNodes.end(), adj);
+                    group[adjIdx] = groupCounter;
+                }
+
                 newGroup.nodes.push_back(adj);
                 toCheck.push(adj);
             }
@@ -56,10 +166,10 @@ std::vector<Group> generateGroups(const Graph &graph) {
     return groups;
 }
 
-Graph generateNeighbour(const Graph &graph, Rng &rng) {
-    const std::vector<Group> groups = generateGroups(graph);
+ProblemGraph generateNeighbour(const ProblemGraph &pGraph, Rng &rng) {
+    const std::vector<Group> groups = generateGroups(pGraph);
 
-    Graph newGraph = graph;
+    ProblemGraph newGraph = pGraph;
 
     std::uniform_int_distribution<unsigned int> nodeDistr(0, groups.size() - 1);
     const Group group = groups[nodeDistr(rng)];
@@ -73,7 +183,7 @@ Graph generateNeighbour(const Graph &graph, Rng &rng) {
     unsigned int color = *it;
 
     for (unsigned int node: group.nodes) {
-        newGraph.color(node, color);
+        newGraph.graph.color(node, color);
     }
 
     return newGraph;
@@ -92,23 +202,30 @@ double coolTemperature(double temperature) {
 unsigned int simulatedAnnealing(Graph &graph, int seed) {
     Rng rng = Rng(seed);
     std::uniform_real_distribution<double> swapDistr(0,1);
-    initRandomColoring(graph, rng);
+    ProblemGraph pGraph(graph);
+
+    FILE* f = fopen("progress.txt", "w");
 
     int maxIter = 1000;
     double temperature = 500;
+    initRandomColoring(pGraph, rng);
 
     const unsigned int nodes = graph.getNbNodes();
-    unsigned int energy = nodes - graph.getHappyVertices();
+    unsigned int energy = nodes - pGraph.getHappyVertices();
 
     for (int i = 0; i < maxIter; ++i) {
-        Graph neighbour = generateNeighbour(graph, rng);
+        ProblemGraph neighbour = generateNeighbour(pGraph, rng);
         unsigned int newEnergy = nodes - neighbour.getHappyVertices();
         if (swapDistr(rng) < swapProbability(energy, newEnergy, temperature)) {
-            printf("Swapped from %u to %u\n", energy, newEnergy);
-            graph = neighbour;
+            fprintf(f, "%u\n", newEnergy);
+            pGraph = neighbour; // FIXME problem
             energy = newEnergy;
         }
         temperature = coolTemperature(temperature);
     }
+    fclose(f);
+
+    pGraph.colorRemoved();
+
     return nodes - energy;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main() {
     Graph graph("graph.txt");
 
-    unsigned int happy = simulatedAnnealing(graph, 123);
+    unsigned int happy = simulatedAnnealing(graph, 1234);
 
     std::cout << happy << std::endl;
 
