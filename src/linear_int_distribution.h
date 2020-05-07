568f1f2f0647a96018dfb1b144a2acf505b4e6f1
Florian Peeters <jeanflo125@gmail.com>
2020-05-07 21:49:46 +0200

    Minor fix for int distribution.

--- src/linear_int_distribution.h
+++ src/linear_int_distribution.h
@@ -3,17 +3,14 @@
 
 #include <random>
 
-using namespace std;
-
 template<class IntType = int>
 class linear_int_distribution {
 
 public:
-    _RNG_REQUIRE_INTTYPE(linear_int_distribution, IntType);
-
     explicit linear_int_distribution(double alpha = 0, IntType min = 0, IntType max = 1) : alpha(alpha),
                                                                                         min(min),
                                                                                         max(max) {
+        std::uniform_int_distribution<IntType> typeCheck(0,1);
         if (alpha > 2)
             this->alpha = 2;
         else if (alpha < -2)
@@ -32,7 +29,7 @@ private:
 
 template<class Engine>
 double linear_distribution(double alpha, Engine &engine) {
-    uniform_real_distribution<double> distribution(0, 1);
+    std::uniform_real_distribution<double> distribution(0, 1);
     return ((sqrt(alpha * alpha - 2. * alpha + 4. * alpha * distribution(engine) + 1.) - 1.) / alpha);
 }
 
@@ -41,7 +38,7 @@ template<class IntType>
 template<class Engine>
 IntType linear_int_distribution<IntType>::operator()(Engine &engine) {
     if (this->alpha == 0) {
-        uniform_int_distribution<IntType> distribution(this->min, this->max - 1);
+        std::uniform_int_distribution<IntType> distribution(this->min, this->max - 1);
         return distribution(engine);
     }
 
