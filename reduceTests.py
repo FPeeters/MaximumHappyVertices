022d7cb3cd79e2d5798cc680fa1eb01a0c8f1bee
Florian Peeters <jeanflo125@gmail.com>
2020-05-14 18:03:55 +0200

    C++ version of the clusteringGenerator, new test script.

--- reduceTests.py
+++ reduceTests.py
@@ -4,8 +4,6 @@ import os
 import time
 from multiprocessing import Pool, Lock
 
-import clusteringGenerator
-
 
 # Adapted from https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
 def print_progress(iteration, total, avg_time=0):
@@ -20,38 +18,40 @@ def print_progress(iteration, total, avg_time=0):
         print()
 
 
-def run_instance(filename, nbNodes, nbColors, preColor, degree, cluster, seed):
+def run_instance(filename, method, nbNodes, nbColors, preColor, degree, alpha, seed):
     t = time.time()
-    gen_result = subprocess.run(["cmake-build-visual-studio\\generator.exe",
-                                 "-n", str(nbNodes), "-k", str(nbColors), "-p", str(preColor),
-                                 "-R", str(degree / (nbNodes - 1.)), "-s", str(seed), "-f", filename],
-                                stdout=subprocess.PIPE, universal_newlines=True)
-
-    # graph = clusteringGenerator.generate_graph(seed, nbNodes, degree / (nbNodes - 1.), cluster, nbColors, preColor)
-    # clusteringGenerator.write_to_file(filename, graph, nbNodes, degree / (nbNodes - 1.), nbColors)
-    # gen = [nbNodes, nbColors, preColor, seed, degree, cluster]
+    if method == "random":
+        gen_result = subprocess.run(["cmake-build-visual-studio\\generator.exe",
+                                     "-n", str(nbNodes), "-k", str(nbColors), "-p", str(preColor),
+                                     "-R", str(degree / (nbNodes - 1.)), "-s", str(seed), "-f", filename],
+                                    stdout=subprocess.PIPE, universal_newlines=True)
+        gen = gen_result.stdout.split("\t")
+        gen = [gen[1], gen[2], float(gen[3]) / nbNodes, gen[7], float(gen[6]) * (nbNodes - 1), alpha]
+    elif method == "cluster":
+        subprocess.run(["cmake-build-visual-studio\\clusteringGenerator.exe", filename, str(nbNodes), str(degree),
+                        str(alpha), str(nbColors), str(preColor), str(seed)])
+        gen = [nbNodes, nbColors, preColor, seed, degree, alpha]
+    else:
+        gen_result = subprocess.run(["cmake-build-visual-studio\\generator.exe",
+                                     "-n", str(nbNodes), "-k", str(nbColors), "-p", str(preColor),
+                                     "-S", str(degree), "-s", str(seed), "-f", filename],
+                                    stdout=subprocess.PIPE, universal_newlines=True)
+        gen = gen_result.stdout.split("\t")
+        gen = [gen[1], gen[2], float(gen[3]) / nbNodes, gen[7], float(gen[6]) * (nbNodes - 1), alpha]
 
     thiruv_result = subprocess.run(["cmake-build-visual-studio\\main.exe", filename, "-red", "thiruvady",
                                     "-a", "greedy"], stdout=subprocess.PIPE, universal_newlines=True)
     basic_result = subprocess.run(["cmake-build-visual-studio\\main.exe", filename, "-red", "basic",
-                                    "-a", "greedy"], stdout=subprocess.PIPE, universal_newlines=True)
+                                   "-a", "greedy"], stdout=subprocess.PIPE, universal_newlines=True)
     articul_result = subprocess.run(["cmake-build-visual-studio\\main.exe", filename, "-red", "articul",
-                                    "-a", "greedy"], stdout=subprocess.PIPE, universal_newlines=True)
+                                     "-a", "greedy"], stdout=subprocess.PIPE, universal_newlines=True)
 
-    try:
-        gen = gen_result.stdout.split("\t")
-        gen[3] = str(float(gen[3]) / nbNodes)
-        gen = gen[1:4] + gen[7:8] + gen[5:7]
-        thiruv = str(sum(map(lambda x: int(x.split(" ")[0]), thiruv_result.stdout.split("\n")[1:4])))
-        basic = str(sum(map(lambda x: int(x.split(" ")[0]), basic_result.stdout.split("\n")[1:5])))
-        articul = str(sum(map(lambda x: int(x.split(" ")[0]), articul_result.stdout.split("\n")[2:6])))
+    thiruv = str(sum(map(lambda x: int(x.split(" ")[0]), thiruv_result.stdout.split("\n")[1:4])))
+    basic = str(sum(map(lambda x: int(x.split(" ")[0]), basic_result.stdout.split("\n")[1:5])))
+    articul = str(sum(map(lambda x: int(x.split(" ")[0]), articul_result.stdout.split("\n")[2:6])))
 
-        os.remove(filename)
-        return gen + [thiruv, basic, articul], time.time() - t
-    except:
-        print("An error occured")
-        return [], time.time() - t
-
+    os.remove(filename)
+    return [method] + gen + [thiruv, basic, articul], time.time() - t
 
 
 def callback(result):
@@ -70,8 +70,7 @@ def callback(result):
 count = 0
 avgTime = -1
 emaFactor = 0
-tunedArgs = ["-a", "simAnn", "-maxI", "5000", "-init", "best", "-temp", "51", "-swap", "0.05", "-split",
-             "0.06"]  # "-time", "30"
+
 threads = 4
 lock = Lock()
 
@@ -80,14 +79,18 @@ if __name__ == '__main__':
     writer = csv.writer(file)
 
     nbNodes_options = [1000]
-    nbColor_options = [5 , 10, 15, 20]
-    preColor_options = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40]
-    degree_options = range(0, 1000)
-    cluster_options = [0]
-    seed_options = [1234, 4321]
+    nbColor_options = [10, 50]
+    preColor_options = [0.05, 0.10, 0.15, 0.20, 0.25]
 
-    nbGraphs = len(nbNodes_options) * len(nbColor_options) * len(preColor_options) * \
-               len(degree_options) * len(cluster_options) * len(seed_options)
+    degree_options = range(0, 51)
+    alpha_options = [-2., -1.5, -1., -0.5, 0, 0.5, 1., 1.5, 2]
+    scale_options = range(1, 26)
+
+    seed_options = [591, 8412, 2107, 959, 3521, 9125, 2276, 2568, 2543, 29, 7131, 564, 2502, 4295, 5309, 2750, 319,
+                    7074, 2605, 9193]
+
+    nbGraphs = len(nbNodes_options) * len(nbColor_options) * len(preColor_options) * (
+                len(degree_options) * (1 + len(alpha_options)) + len(scale_options)) * len(seed_options)
     emaFactor = 2 / (nbGraphs / len(nbNodes_options) + 1)
 
     print("Total graphs:", nbGraphs)
@@ -99,9 +102,21 @@ if __name__ == '__main__':
     for nbNodes in nbNodes_options:
         for nbColors in nbColor_options:
             for preColor in preColor_options:
-                for degree in degree_options:
-                    for cluster in cluster_options:
-                        for seed in seed_options:
+                for seed in seed_options:
+                    for degree in degree_options:
+                        fileCount += 1
+                        if preColor * nbNodes < nbColors:
+                            writer.writerow([nbNodes, nbColors, nbNodes * preColor, seed])
+                            count += 1
+                            print_progress(count, nbGraphs, avgTime)
+                            continue
+
+                        filename = "todo/graph" + str(fileCount) + ".txt"
+                        pool.apply_async(run_instance,
+                                         (filename, "random", nbNodes, nbColors, preColor, degree, 0, seed),
+                                         callback=callback)
+
+                        for alpha in alpha_options:
                             fileCount += 1
                             if preColor * nbNodes < nbColors:
                                 writer.writerow([nbNodes, nbColors, nbNodes * preColor, seed])
@@ -111,8 +126,21 @@ if __name__ == '__main__':
 
                             filename = "todo/graph" + str(fileCount) + ".txt"
                             pool.apply_async(run_instance,
-                                             (filename, nbNodes, nbColors, preColor, degree, cluster, seed),
+                                             (filename, "cluster", nbNodes, nbColors, preColor, degree, alpha, seed),
                                              callback=callback)
+
+                    for scale in scale_options:
+                        fileCount += 1
+                        if preColor * nbNodes < nbColors:
+                            writer.writerow([nbNodes, nbColors, nbNodes * preColor, seed])
+                            count += 1
+                            print_progress(count, nbGraphs, avgTime)
+                            continue
+
+                        filename = "todo/graph" + str(fileCount) + ".txt"
+                        pool.apply_async(run_instance,
+                                         (filename, "scale", nbNodes, nbColors, preColor, scale, 0, seed),
+                                         callback=callback)
 
     pool.close()
     pool.join()
---
+++ src/clusteringGenerator.cpp
@@ -0,0 +1,142 @@
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include <fstream>
+#include "linear_int_distribution.h"
+
+typedef std::mt19937_64 Rng;
+
+struct adjacency_item {
+    unsigned int index;
+    std::vector<unsigned int> adjacency;
+    unsigned int color = 0;
+};
+
+bool compare(const adjacency_item &first, const adjacency_item &second) {
+    return first.adjacency.size() < second.adjacency.size();
+}
+
+bool compareReverse(const adjacency_item &first, const adjacency_item &second) {
+    if (first.adjacency.size() == second.adjacency.size())
+        return first.index < second.index;
+    return first.adjacency.size() > second.adjacency.size();
+}
+
+int main(int argc, char **argv) {
+    if (argc != 8) {
+        std::cout << "USAGE: filename nbNodes avgDegree alpha nbColors precolor seed" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    char *filename = argv[1];
+    int nbNodes = (int) strtol(argv[2], nullptr, 10);
+    double avgDegree = strtod(argv[3], nullptr);
+    double alpha = strtod(argv[4], nullptr);
+    int nbColors = (int) strtol(argv[5], nullptr, 10);
+    double precolor = strtod(argv[6], nullptr);
+    long seed = strtol(argv[7], nullptr, 10);
+
+    int nbPrecolor = (int) round(nbNodes * precolor);
+    if (nbPrecolor < nbColors) {
+        std::cout << "At least nbColor nodes must be precolored" << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (avgDegree > nbNodes - 1) {
+        std::cout << "The maximum average degree is nbNodes-1" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    Rng rng(seed);
+
+    std::vector<adjacency_item> adjacency(nbNodes);
+    int i = 0;
+    for (adjacency_item &item: adjacency)
+        item.index = i++;
+
+    int nbEdges = (int) round(avgDegree * nbNodes / 2.);
+
+    int nbFilled = 0;
+
+    for (int j = 0; j < nbEdges; ++j) {
+        int curr_range = nbNodes - nbFilled;
+        linear_int_distribution<int> firstDistr(alpha, 0, curr_range);
+        int first = firstDistr(rng);
+        linear_int_distribution<int> secondDistr(alpha, 0, curr_range - 1);
+        int second = secondDistr(rng);
+        if (second >= first)
+            second += 1;
+
+        int dir = alpha > 0 ? 1 : -1;
+
+        int start = second;
+        std::vector<unsigned int> &firstAdj = adjacency[first].adjacency;
+        while (second == first ||
+               std::find(firstAdj.begin(), firstAdj.end(), adjacency[second].index) != firstAdj.end()) {
+            second += dir;
+            if (second == nbNodes) {
+                second = start - 1;
+                dir = -1;
+            } else if (second == -1) {
+                second = start + 1;
+                dir = 1;
+            }
+        }
+
+        std::vector<unsigned int> &secondAdj = adjacency[second].adjacency;
+
+        firstAdj.push_back(adjacency[second].index);
+        secondAdj.push_back(adjacency[first].index);
+
+        if (firstAdj.size() == nbNodes - 1)
+            ++nbFilled;
+        if (secondAdj.size() == nbNodes - 1)
+            ++nbFilled;
+
+        std::shuffle(adjacency.begin(), adjacency.end(), rng);
+        std::sort(adjacency.begin(), adjacency.end(), compare);
+    }
+
+    std::vector<int> renames(nbNodes);
+
+    i = 0;
+    for (const adjacency_item &item: adjacency)
+        renames[item.index] = i++;
+
+    for (adjacency_item &item: adjacency) {
+        item.index = nbNodes - renames[item.index];
+        for (unsigned int &j : item.adjacency)
+            j = nbNodes - renames[j];
+        std::sort(item.adjacency.begin(), item.adjacency.end());
+    }
+
+    std::shuffle(adjacency.begin(), adjacency.end(), rng);
+
+    for (unsigned int col = 0; col < nbColors; ++col)
+        adjacency[col].color = col + 1;
+
+    std::uniform_int_distribution colorDistr(1, nbColors);
+    for (unsigned int j = nbColors; j < nbPrecolor; ++j)
+        adjacency[j].color = colorDistr(rng);
+
+    std::sort(adjacency.begin(), adjacency.end(), compareReverse);
+
+    std::ofstream out;
+    out.open(filename);
+
+    out << "c This graph was made by the clusteringGenerator of F. Peeters" << std::endl;
+    out << "c Using these settings: " << nbNodes << " " << avgDegree << " " << alpha << " " << nbColors << " " << precolor << " " << seed << std::endl;
+    out << "p edge " << nbNodes << " " << nbEdges << " " << nbColors << std::endl;
+
+    for (const adjacency_item &item: adjacency) {
+        for (unsigned int adj: item.adjacency) {
+            if (item.index < adj)
+                out << "e " << item.index << " " << adj << std::endl;
+        }
+    }
+
+    for (const adjacency_item &item: adjacency) {
+        if (item.color != 0)
+            out << "n " << item.index << " " << item.color << std::endl;
+    }
+    out.close();
+}
